9a9e0af6b0d6015f601d744b6722a5f74548b3a4
Chanon Roy <chanonroy@gmail.com>
2021-05-08 14:23:44 +0100

    fix SSR issue

--- src/hooks/use-window-dimensions.ts
+++ src/hooks/use-window-dimensions.ts
@@ -1,26 +1,32 @@
+// useWindowDimension.js
 import { useState, useEffect } from 'react'
 
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window
-  return {
-    width,
-    height,
-  }
-}
-
 export default function useWindowDimensions() {
+  const hasWindow = typeof window !== 'undefined'
+
+  function getWindowDimensions() {
+    const width = hasWindow ? window.innerWidth : null
+    const height = hasWindow ? window.innerHeight : null
+    return {
+      width,
+      height,
+    }
+  }
+
   const [windowDimensions, setWindowDimensions] = useState(
     getWindowDimensions()
   )
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions())
-    }
+  const handleResize = () => {
+    setWindowDimensions(getWindowDimensions())
+  }
 
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
+  useEffect(() => {
+    if (hasWindow) {
+      window.addEventListener('resize', handleResize)
+      return () => window.removeEventListener('resize', handleResize)
+    }
+  }, [hasWindow])
 
   return windowDimensions
 }
